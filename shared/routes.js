7646aae7acbc26ae1f049ed165a4322ad30a758e
Patrick Templeton <contact@patricktempleton.xyz>
2016-01-02 09:16:42 -0500

    Fixed weird/inconsistant spacing and layout.

--- shared/routes.js
+++ shared/routes.js
@@ -1,35 +1,59 @@
-  // set up the main template the the router will use to build pages
-  Router.configure({
+  Router.configure({ // set up main template router will use to build pages
     layoutTemplate: 'ApplicationLayout'
   });
-  // specify the top level route, the page users see when they arrive at the site
-  Router.route('/', function () {
+
+
+  Router.route('/', function () { // specify top level route, the page users see when arriving at site
     console.log("rendering root /");
-    this.render("navbar", {to:"header"});
-    this.render("lobby_page", {to:"main"});  
+    this.render("navbar", {
+      to: "header"
+    });
+    this.render("lobby_page", {
+      to: "main"
+    });
   });
 
-  // specify a route that allows the current user to chat to another users
-  Router.route('/chat/:_id', function () {
+
+  Router.route('/chat/:_id', function () { // specify route that allows current user to chat with another user
+    
     // the user they want to chat to has id equal to 
     // the id sent in after /chat/... 
     var otherUserId = this.params._id;
+    
     // find a chat that has two users that match current user id
     // and the requested user id
-    var filter = {$or:[
-                {user1Id:Meteor.userId(), user2Id:otherUserId}, 
-                {user2Id:Meteor.userId(), user1Id:otherUserId}
-                ]};
+    var filter = {
+      $or: [
+        {
+          user1Id: Meteor.userId(),
+          user2Id: otherUserId
+        },
+        {
+          user2Id: Meteor.userId(),
+          user1Id: otherUserId
+        }
+      ]
+    };
+    
     var chat = Chats.findOne(filter);
-    if (!chat){// no chat matching the filter - need to insert a new one
-      chatId = Chats.insert({user1Id:Meteor.userId(), user2Id:otherUserId});
-    }
-    else {// there is a chat going already - use that. 
+    if (!chat) { // no chat matching the filter - need to insert a new one
+      chatId = Chats.insert({
+        user1Id: Meteor.userId(),
+        user2Id: otherUserId
+      });
+    } else { // there is a chat going already - use that. 
       chatId = chat._id;
     }
-    if (chatId){// looking good, save the id to the session
-      Session.set("chatId",chatId);
+    
+    if (chatId) { // save the id to the session
+      Session.set("chatId", chatId);
     }
-    this.render("navbar", {to:"header"});
-    this.render("chat_page", {to:"main"});  
+    
+    this.render("navbar", {
+      to: "header"
+    });
+    this.render("chat_page", {
+      to: "main"
+    });
+    
   });
