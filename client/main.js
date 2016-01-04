326b9e7aa3548d86fae1ffff9ab5292951806e4c
Patrick Templeton <contact@patricktempleton.xyz>
2016-01-04 14:49:49 -0500

    Added about page and fixed code formatting.

--- client/main.js
+++ client/main.js
@@ -1,130 +1,110 @@
-Meteor.subscribe('chats', function(){
+// Subsriptions
+
+Meteor.subscribe('chats', function () {
   return Chats.find().fetch();
 });
 
-Meteor.subscribe('userData', function(){
+Meteor.subscribe('userData', function () {
   return Meteor.users.find().fetch();
 });
 
-  ///
-  // helper functions
-  ///
+// Helper functions
 
-  Template.available_user_list.helpers({
-    users: function () {
-      return Meteor.users.find();
+Template.available_user_list.helpers({
+  users: function () {
+    return Meteor.users.find();
+  }
+});
+
+Template.available_user.helpers({
+  getUsername: function (userId) {
+    var user = Meteor.users.findOne({
+      _id: userId
+    });
+    return user.profile.username;
+  },
+
+  isMyUser: function (userId) {
+    if (userId == Meteor.userId()) {
+      return true;
+    } else {
+      return false;
     }
-  });
+  }
 
-  Template.available_user.helpers({
-    getUsername: function (userId) {
-      var user = Meteor.users.findOne({
-        _id: userId
-      });
-      return user.profile.username;
-    },
+});
 
-    isMyUser: function (userId) {
-      if (userId == Meteor.userId()) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+Template.chat_page.helpers({
 
-  });
+  messages: function () {
+    var chat = Chats.findOne({
+      _id: Session.get("chatId")
+    });
+    return chat.messages;
+  },
 
-  Template.chat_page.helpers({
+  other_user_name: function () {
+    var otherUser;
+    var chat = Chats.findOne({
+      _id: Session.get("chatId")
+    });
 
-    messages: function () {
-      var chat = Chats.findOne({
-        _id: Session.get("chatId")
-      });
-      return chat.messages;
-    },
-
-    other_user_name: function () {
-      var otherUser;
-      var chat = Chats.findOne({
-        _id: Session.get("chatId")
-      });
-
-      if (chat.user1Id != Meteor.userId()){
-        otherUser = chat.user1Id;
-      } else {
-        otherUser = chat.user2Id;
-      }
-      
-      return Meteor.users.findOne({_id: otherUser}).profile.username;
-    },
-    
-    other_user_avatar: function () {
-      var otherUser;
-      var chat = Chats.findOne({
-        _id: Session.get("chatId")
-      });
-
-      if (chat.user1Id != Meteor.userId()){
-        otherUser = chat.user1Id;
-      } else {
-        otherUser = chat.user2Id;
-      }
-      
-      return Meteor.users.findOne({_id: otherUser}).profile.avatar;
+    if (chat.user1Id != Meteor.userId()) {
+      otherUser = chat.user1Id;
+    } else {
+      otherUser = chat.user2Id;
     }
 
-  });
+    return Meteor.users.findOne({
+      _id: otherUser
+    }).profile.username;
+  },
 
-  Template.chat_message.helpers({
+  other_user_avatar: function () {
+    var otherUser;
+    var chat = Chats.findOne({
+      _id: Session.get("chatId")
+    });
 
-    getUserById: function (sentBy) {
-      return Meteor.users.findOne({
-        _id: sentBy
-      }).profile.username;
-    },
-
-    messageBackgroundClass: function (sentBy) {
-      if (sentBy == Meteor.userId()) {
-        return "bg-success";
-      } else {
-        return "bg-info";
-      }
+    if (chat.user1Id != Meteor.userId()) {
+      otherUser = chat.user1Id;
+    } else {
+      otherUser = chat.user2Id;
     }
 
-  });
+    return Meteor.users.findOne({
+      _id: otherUser
+    }).profile.avatar;
+  }
 
-  Template.chat_page.events({
+});
 
-    'submit .js-send-chat': function (event) { // this event fires when the user sends a message on the chat page
-      event.preventDefault(); // stop the form from triggering a page reload
-      /*
-      var chat = Chats.findOne({ // see if we can find a chat object in the database to which we'll add the message
-        _id: Session.get("chatId")
-      });
+Template.chat_message.helpers({
 
-      if (chat) { // ok - we have a chat to use
-        var msgs = chat.messages; // pull the messages property
-        if (!msgs) { // no messages yet, create a new array
-          msgs = [];
-        }
+  getUserById: function (sentBy) {
+    return Meteor.users.findOne({
+      _id: sentBy
+    }).profile.username;
+  },
 
-        // is a good idea to insert data straight from the form
-        // (i.e. the user) into the database?? certainly not. 
-        // push adds the message to the end of the array
-
-        msgs.push({
-          timeStamp: new Date().toUTCString(),
-          sentBy: Meteor.user()._id,
-          text: event.target.chat.value
-        });
-
-        event.target.chat.value = ""; // reset the form
-        chat.messages = msgs; // put the messages array onto the chat object
-        Chats.update(chat._id, chat); // update the chat object in the database.
-      }
-      */
-      Meteor.call('sendMessage', Session.get("chatId"), event.target.chat.value);
-      event.target.chat.value = ""; // reset the form
+  messageBackgroundClass: function (sentBy) {
+    if (sentBy == Meteor.userId()) {
+      return "bg-success";
+    } else {
+      return "bg-info";
     }
+  }
 
-  });
+});
+
+// Events
+
+Template.chat_page.events({
+
+  'submit .js-send-chat': function (event) { // this event fires when the user sends a message on the chat page
+    event.preventDefault(); // stop the form from triggering a page reload
+    Meteor.call('sendMessage', Session.get("chatId"), event.target.chat.value);
+    event.target.chat.value = ""; // reset the form
+  }
+
+});
--- shared/routes.js
+++ shared/routes.js
@@ -1,78 +1,87 @@
-  Router.configure({ // set up main template router will use to build pages
-    layoutTemplate: 'ApplicationLayout'
+Router.configure({ // set up main template router will use to build pages
+  layoutTemplate: 'ApplicationLayout'
+});
+
+
+Router.route('/', function () { // specify top level route, the page users see when arriving at site
+  //console.log("rendering root /");
+  var route = this;
+
+  route.render("navbar", {
+    to: "header"
   });
 
-
-  Router.route('/', function () { // specify top level route, the page users see when arriving at site
-    //console.log("rendering root /");
-    var route = this;
-
-    route.render("navbar", {
-      to: "header"
-    });
-
-    if (Meteor.user()) {
-      route.render("lobby_page", {
-        to: "main"
-      });
-    } else {
-      route.render("landing_page", {
-        to: "main"
-      });
-    }
-  });
-
-  Router.route('/chat/:_id', function () { // specify route that allows current user to chat with another user
-    var route = this;
-    /*    
-    // the user they want to chat to has id equal to 
-    // the id sent in after /chat/... 
-    var otherUserId = this.params._id;
-    
-    // find a chat that has two users that match current user id
-    // and the requested user id
-    var filter = {
-      $or: [
-        {
-          user1Id: Meteor.userId(),
-          user2Id: otherUserId
-        },
-        {
-          user2Id: Meteor.userId(),
-          user1Id: otherUserId
-        }
-      ]
-    };
-    
-    var chat = Chats.findOne(filter);
-    if (!chat) { // no chat matching the filter - need to insert a new one
-      chatId = Chats.insert({
-        user1Id: Meteor.userId(),
-        user2Id: otherUserId
-      });
-    } else { // there is a chat going already - use that. 
-      chatId = chat._id;
-    }
-    */
-
-    route.render("navbar", {
-      to: "header"
-    });
-
-    route.render("loading", {
+  if (Meteor.user()) {
+    route.render("lobby_page", {
       to: "main"
     });
-
-    Session.set('chatId', ""); // clear chatId
-
-    Meteor.call('getChat', this.params._id, function (err, res) {
-      if (err) { // error
-        console.log("getChat error...");
-      } else {
-        Session.set("chatId", res);
-        route.render("chat_page", {
-          to: "main"
-        });
-      }
+  } else {
+    route.render("landing_page", {
+      to: "main"
     });
+  }
+});
+
+Router.route('/chat/:_id', function () { // specify route that allows current user to chat with another user
+  var route = this;
+  /*    
+  // the user they want to chat to has id equal to 
+  // the id sent in after /chat/... 
+  var otherUserId = this.params._id;
+
+  // find a chat that has two users that match current user id
+  // and the requested user id
+  var filter = {
+    $or: [
+      {
+        user1Id: Meteor.userId(),
+        user2Id: otherUserId
+      },
+      {
+        user2Id: Meteor.userId(),
+        user1Id: otherUserId
+      }
+    ]
+  };
+
+  var chat = Chats.findOne(filter);
+  if (!chat) { // no chat matching the filter - need to insert a new one
+    chatId = Chats.insert({
+      user1Id: Meteor.userId(),
+      user2Id: otherUserId
+    });
+  } else { // there is a chat going already - use that. 
+    chatId = chat._id;
+  }
+  */
+
+  route.render("navbar", {
+    to: "header"
   });
+
+  route.render("loading", {
+    to: "main"
+  });
+
+  Session.set('chatId', ""); // clear chatId
+
+  Meteor.call('getChat', this.params._id, function (err, res) {
+    if (err) { // error
+      console.log("getChat error...");
+    } else {
+      Session.set("chatId", res);
+      route.render("chat_page", {
+        to: "main"
+      });
+    }
+  });
+});
+
+Router.route('/about/', function () {
+  this.render("navbar", {
+    to: "header"
+  });
+  this.render("about", {
+    to: "main"
+  });
+});
